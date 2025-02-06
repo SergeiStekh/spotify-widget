ace242ecd0fd6d45c9a3706e88cfb2c14938a42d
Sergei Stekh <sergeysteh@gmail.com>
2025-02-06 09:21:26 +0100

    add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import SpotifyWidget from "@/components/SpotifyWidget"
+import { Suspense } from 'react';
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-900">
-      <SpotifyWidget />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex justify-center items-center min-h-screen bg-gray-900">
+        <SpotifyWidget />
+      </div>
+    </Suspense>
   );
 };
 
